22d99af473483e95e931fa58261efe08a612aab4
Muhammad Asad Ullah <masadullahkhanhfd@gmail.com>
2025-09-21 11:30:06 +0500

    Some changes in Server

--- Server.js
+++ Server.js
@@ -5,15 +5,24 @@ const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
 dotenv.config();
 const app = express();
-const path = require("path")
+const path = require("path");
+// app.use(
+//   cors({
+//     // origin: [
+//     //   "http://localhost:5173",
+//     //   "https://inventory-and-sales-management-system-backend-production.up.railway.app",
+//     // ],
+//     origin: "*",
+//     credentials: true,
+//   })
+// );
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true, 
+  origin: '*', 
 }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 const authRoutes = require("./routes/authRoutes");
 app.use("/api/auth", authRoutes);
@@ -28,26 +37,26 @@ const orderRoutes = require("./routes/orderRoutes");
 app.use("/api/orders", orderRoutes);
 
 const userRoutes = require("./routes/userRoutes");
-app.use('/api/users', userRoutes);
+app.use("/api/users", userRoutes);
 
 const formRoutes = require("./routes/formRoutes");
-app.use("/api/contact", formRoutes)
+app.use("/api/contact", formRoutes);
 
 const cartRoutes = require("./routes/cartRoutes");
-app.use("/api/cart", cartRoutes)
+app.use("/api/cart", cartRoutes);
 
 app.get("/test-error", (req, res) => {
   res.status(400).json({ success: false, error: "Test error message" });
 });
 
 const multerFile = require("./images/multerFile");
-app.use('/api/images', multerFile);
+app.use("/api/images", multerFile);
 
-const errorHandler = require("./middlewares/errorHandler")
+const errorHandler = require("./middlewares/errorHandler");
 app.use(errorHandler);
 
 connectDB();
 
-app.listen(port, ()=>{
-    console.log(`Server is running on port: ${port}`);
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
 });
